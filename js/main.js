85cc4d11377c79f932bedbfbcd5090f571a11be1
Jorge Cosgayon <jcosgayon.pdpi@gmail.com>
2023-07-18 05:06:18 +0800

    Tentative structure

--- js/main.js
+++ js/main.js
@@ -3,12 +3,46 @@ const { createApp, ref } = Vue
 createApp({
     setup() {
         const message = ref('Hello vue!')
-        function test() {
-            alert('sadg')
+
+        const dataCenters = ref([
+            {label: "Local", url: "./data.json"},
+            {label: "Mock", url: "https://dummyjson.com/products/"},
+            {label: "Dev", url: "http://pcr005.scl.five9.com"},
+            {label: "Sta. Clara", url: "http://pcr004.scl.five9.com"},
+            {label: "Atlanta", url: "http://pcr001.atl.five9.com/"},
+        ])
+
+        let dataCenterKey = ref(0)
+
+        let isResponseLoading = ref(false)
+        let responseMessage = ref("")
+        let responseData = ref(null)
+
+        async function fetchData() {
+            this.responseData = null
+            this.responseMessage = ""
+            const dataCenter = this.dataCenters[this.dataCenterKey];
+            this.isResponseLoading = true
+            try {
+                const response = await fetch(dataCenter.url)
+                this.responseMessage = "Success"
+                this.responseData = await response.json()
+            } catch (error) {
+                this.responseMessage = error
+            }
+            this.isResponseLoading = false
         }
+
         return {
+            dataCenters,
+            dataCenterKey,
             message,
-            test
+            isResponseLoading,
+            responseData,
+            responseMessage,
+
+            // methods
+            fetchData
         }
     }
 }).mount('#app')
